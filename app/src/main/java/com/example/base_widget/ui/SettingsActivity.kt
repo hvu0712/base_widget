d998d75ac3c6cebfc6994652356e5643cb887c17
LG-Team <lg.app.dev.team@gmail.com>
2024-01-04 15:45:39 +0700

    rebase from master

---
+++ app/src/main/java/com/example/base_widget/ui/SettingsActivity.kt
@@ -0,0 +1,116 @@
+package com.example.base_widget.ui
+
+import android.content.Intent
+import android.net.Uri
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.SystemClock
+import android.widget.Toast
+import com.example.base_widget.R
+import com.example.base_widget.base.BaseActivity
+import com.example.base_widget.base.LanguageScreenActivity
+import com.example.base_widget.common.SharePrefUtils
+import com.example.base_widget.common.hide
+import com.example.base_widget.common.setOnSingleClickListener
+import com.example.base_widget.databinding.ActivitySettingsBinding
+import com.example.base_widget.ui.dialog.RatingDialog
+import com.google.android.play.core.review.ReviewInfo
+import com.google.android.play.core.review.ReviewManager
+import com.google.android.play.core.review.ReviewManagerFactory
+import com.google.android.play.core.tasks.Task
+
+class SettingsActivity : BaseActivity<ActivitySettingsBinding>() {
+
+    private var lastClickTime: Long = 0
+    private var ratingDialog: RatingDialog? = null
+
+    override fun inflateViewBinding() = ActivitySettingsBinding.inflate(layoutInflater)
+
+    override fun initView() {
+        ratingDialog = RatingDialog(this)
+        initRateDialog()
+    }
+
+    override fun setUpListener() {
+        binding.apply {
+            btnBack.setOnSingleClickListener {
+                onBackPressedDispatcher.onBackPressed()
+            }
+            llLanguage.setOnSingleClickListener {
+                showActivity(LanguageScreenActivity::class.java, null)
+            }
+            llRate.setOnSingleClickListener {
+                ratingDialog?.showDialog()
+            }
+            llShare.setOnSingleClickListener {
+                shareApp()
+            }
+            llPrivacy.setOnSingleClickListener {
+                startActivity(Intent(Intent.ACTION_VIEW, Uri.parse("https://lg.taurusplay.store/privacy-policy")))
+            }
+        }
+    }
+
+    private fun shareApp() {
+        if (SystemClock.elapsedRealtime() - lastClickTime < 1000){
+            return
+        }
+        lastClickTime = SystemClock.elapsedRealtime()
+        val shareIntent = Intent(Intent.ACTION_SEND)
+        shareIntent.type = "text/plain"
+        shareIntent.putExtra(Intent.EXTRA_SUBJECT, getString(R.string.app_name))
+        val shareMessage =
+            "${getString(R.string.app_name)} \n https://play.google.com/store/apps/details?id=${packageName}"
+        shareIntent.putExtra(Intent.EXTRA_TEXT, shareMessage)
+//        AppOpenManager.getInstance().disableAppResumeWithActivity(HomeActivity::class.java)
+        startActivity(Intent.createChooser(shareIntent, getString(R.string.share_to)))
+    }
+
+    private fun initRateDialog() {
+        ratingDialog?.setCancelable(true)
+        ratingDialog?.init(this, object : RatingDialog.OnPress {
+            override fun send() {
+                ratingDialog?.dismiss()
+                Toast.makeText(
+                    applicationContext,
+                    getString(R.string.rate_thanks),
+                    Toast.LENGTH_SHORT
+                ).show()
+                SharePrefUtils.isRated = true
+                binding.llRate.hide()
+            }
+
+            override fun rating() {
+                rateInApp()
+            }
+
+            override fun cancel() {
+                ratingDialog?.dismiss()
+            }
+
+            override fun later() {
+                ratingDialog?.dismiss()
+            }
+
+            override fun dismissDialog() {
+
+            }
+        })
+    }
+
+    private fun rateInApp() {
+        val manager: ReviewManager = ReviewManagerFactory.create(this)
+        val request: Task<ReviewInfo> = manager.requestReviewFlow()
+        request.addOnCompleteListener { task ->
+            if (task.isSuccessful) {
+                val reviewInfo: ReviewInfo = task.result
+                val flow: Task<Void> = manager.launchReviewFlow(this, reviewInfo)
+                flow.addOnCompleteListener {
+                    SharePrefUtils.isRated = true
+                    ratingDialog?.dismiss()
+                    binding.llRate.hide()
+                }
+            }
+        }
+    }
+}
--- app/src/main/java/com/example/base_widget/ui/home/HomeActivity.kt
+++ app/src/main/java/com/example/base_widget/ui/home/HomeActivity.kt
@@ -1,9 +1,18 @@
 package com.example.base_widget.ui.home
 
+import android.graphics.drawable.Drawable
+import android.util.Log
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.DataSource
+import com.bumptech.glide.load.engine.GlideException
+import com.bumptech.glide.request.RequestListener
+import com.bumptech.glide.request.target.Target
+import com.example.base_widget.R
 import android.content.Intent
 import com.example.base_widget.base.BaseActivity
 import com.example.base_widget.common.setOnClickAffect
 import com.example.base_widget.databinding.ActivityHomeBinding
+import com.example.base_widget.ui.SettingsActivity
 import com.example.base_widget.ui.shop.AllShopActivity
 import com.example.base_widget.ui.shop.PlantShopActivity
 
@@ -12,7 +21,7 @@ class HomeActivity: BaseActivity<ActivityHomeBinding>() {
     override fun inflateViewBinding() = ActivityHomeBinding.inflate(layoutInflater)
 
     override fun initView() {
-
+        Glide.with(this@HomeActivity).asGif().placeholder(R.drawable.iv_plant).load(R.raw.plant_animation).into(binding.ivPlant)
     }
 
     override fun setUpListener() {
