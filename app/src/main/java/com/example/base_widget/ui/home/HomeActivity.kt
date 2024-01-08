6fc8944d23064ac6fc85b7fa990a17a656f06d2a
LG-Team <lg.app.dev.team@gmail.com>
2024-01-08 15:33:49 +0700

    resolve conflict

--- app/src/main/java/com/example/base_widget/ui/home/HomeActivity.kt
+++ app/src/main/java/com/example/base_widget/ui/home/HomeActivity.kt
@@ -1,26 +1,11 @@
 package com.example.base_widget.ui.home
 
-import android.graphics.drawable.Drawable
-import android.util.Log
 import com.bumptech.glide.Glide
-import com.bumptech.glide.load.DataSource
-import com.bumptech.glide.load.engine.GlideException
-import com.bumptech.glide.request.RequestListener
-import com.bumptech.glide.request.target.Target
-import com.example.base_widget.R
-import android.graphics.drawable.Drawable
-import android.util.Log
-import com.bumptech.glide.Glide
-import com.bumptech.glide.load.DataSource
-import com.bumptech.glide.load.engine.GlideException
-import com.bumptech.glide.request.RequestListener
-import com.bumptech.glide.request.target.Target
 import com.example.base_widget.R
 import android.content.Intent
 import com.example.base_widget.base.BaseActivity
 import com.example.base_widget.common.setOnClickAffect
 import com.example.base_widget.databinding.ActivityHomeBinding
-import com.example.base_widget.ui.SettingsActivity
 import com.example.base_widget.ui.SettingsActivity
 import com.example.base_widget.ui.shop.AllShopActivity
 import com.example.base_widget.ui.shop.PlantShopActivity
